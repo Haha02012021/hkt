71904d7f01977bd9b3d73e714fbce525d48a7525
bach-long <nguyenbach19122002@gmail.com>
2022-10-07 17:28:22 +0700

    feat(comment): add comment api and fix some tables errors

--- backend/database/migrations/2022_10_07_032324_create_posts_table.php
+++ backend/database/migrations/2022_10_07_032324_create_posts_table.php
@@ -16,9 +16,10 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('content');
-            $table->integer('type')->comment('0: chia se, 1: hoi dap');
+            $table->tinyInteger('type')->comment('0: chia se, 1: hoi dap');
             $table->bigInteger('class_id')->nullable();
             $table->bigInteger('user_id');
+            $table->tinyInteger('completed')->nullable()->comment('0:chua xong, 1:da xong');
             $table->timestamps();
         });
     }
--- backend/database/migrations/2022_10_07_072209_create_images_table.php
+++ backend/database/migrations/2022_10_07_072209_create_images_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('post_id');
+            $table->string('link');
             $table->timestamps();
         });
     }
