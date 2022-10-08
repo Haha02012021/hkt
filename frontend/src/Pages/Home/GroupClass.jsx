2af7204b05871f691f42f8c8bb94d6139fa60aab
TrinhChung <chungtrinh2k2@gmail.com>
2022-10-08 11:33:51 +0700

    finish modal add class

--- frontend/src/Pages/Home/GroupClass.jsx
+++ frontend/src/Pages/Home/GroupClass.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button } from "@mui/material";
 import CardClass from "../../Components/Page/CardClass";
 import FactCheckIcon from "@mui/icons-material/FactCheck";
+import { useSelector } from "react-redux";
+import ControlPointIcon from "@mui/icons-material/ControlPoint";
+import ModalPostBlog from "../../Components/Page/ModalAddClass";
 
 const GroupClass = () => {
+  const [createPostModalOpen, setCreatePostModalOpen] = useState(false);
+
+  const infoUser = useSelector((state) => state.user.infoUser);
   const classes = [
     { name: "Bong chuyen hoi", nameTeacher: "nameTeacher" },
     { name: "Tieng Nhat", nameTeacher: "nameTeacher" },
@@ -21,12 +27,22 @@ const GroupClass = () => {
           <FactCheckIcon sx={{ marginRight: "5px" }} />
           Việc cần làm
         </Button>
+        {infoUser.role === 1 ? (
+          <Button onClick={() => setCreatePostModalOpen(true)}>
+            <ControlPointIcon sx={{ marginRight: "2px" }} />
+            Thêm lớp học
+          </Button>
+        ) : null}
       </Box>
       <Box sx={{ marginTop: "20px", display: "flex", gap: "20px" }}>
         {classes.map((classItem) => {
           return <CardClass item={classItem} />;
         })}
       </Box>
+      <ModalPostBlog
+        open={createPostModalOpen}
+        onClose={setCreatePostModalOpen}
+      />
     </Box>
   );
 };
