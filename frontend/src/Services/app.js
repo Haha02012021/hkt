38e3134970052a7a2ddd5283ce9e00e5e1278312
TrinhChung <chungtrinh2k2@gmail.com>
2022-10-08 13:22:25 +0700

    add handleApi and refactor service

--- frontend/src/Services/app.js
+++ frontend/src/Services/app.js
@@ -5,19 +5,30 @@ const handleGetPostApi = (type, page) => {
 };
 
 const handleLikePostApi = (id) => {
-  return axios.post(`http://127.0.0.1:8000/api/reaction/post/${id}`);
+  return axios.post(`/api/reaction/post/${id}`);
 };
 
 const handleCompleteQuestionApi = (id) => {
-  return axios.post(`http://127.0.0.1:8000/api/post/complete/${id}`);
+  return axios.post(`/api/post/complete/${id}`);
 };
 
 const handleCommentsPostApi = (id) => {
-  return axios.get(`http://127.0.0.1:8000/api/comments/post/${id}`,);
+  return axios.get(`/api/comments/post/${id}`);
 };
 
 const handleCommentApi = (req) => {
-  return axios.post(`http://127.0.0.1:8000/api/comments/add`, req);
-}
+  return axios.post(`/api/comments/add`, req);
+};
 
-export { handleGetPostApi, handleLikePostApi, handleCommentsPostApi, handleCommentApi, handleCompleteQuestionApi };
+const handleGetOtherUsersApi = () => {
+  return axios.post(`/api/user/get-other-users`);
+};
+
+export {
+  handleGetPostApi,
+  handleLikePostApi,
+  handleCommentsPostApi,
+  handleCommentApi,
+  handleCompleteQuestionApi,
+  handleGetOtherUsersApi,
+};
